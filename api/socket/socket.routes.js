e31601e9b911b724996b163081d2555b53718398
AbirNadav <pakakaw41@gmail.com>
2020-05-24 23:20:33 +0300

    123123

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -4,16 +4,37 @@ module.exports = connectSockets
 function connectSockets(io) {
 
     io.on('connection', socket => {
-        socket.on('doRefresh', msg=>{
+        socket.on('doRefresh', msg => {
             io.to(socket.boardId).emit('doRefresh', msg)
         })
-        socket.on('boardViewed', boardId=>{
-            console.log('i reecived boardId' , boardId)
-            if (socket.boardId) {
-                socket.leave(socket.boarId)
+        // socket.on('boardViewed', boardId => {
+        //     console.log('i reecived boardId', boardId)
+        //     if (socket.boardId) {
+        //         socket.leave(socket.boarId)
+        //     }
+        //     socket.join(boardId)
+        //     socket.boardId = boardId;
+        // })
+
+        socket.on('openBoardChat', data => {
+
+
+            console.log('got messege', data.msgData.text)
+            console.log('Someone Joined Board chat!', data.boardChatId)
+            if (socket.boardChatId) {
+
+                socket.leave(socket.boardChatId)
             }
-            socket.join(boardId)
-            socket.boardId = boardId;
+            socket.join(data.boardChatId)
+            socket.boardChatId = data.boardChatId;
+            if (data.msgData)
+                socket.broadcast.emit('openBoardChat', data)
         })
+
+
+
+
+
+
     })
 }
