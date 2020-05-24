b6689e754f6e8f56d9bdd2d62f856d4dae875867
shahar <shaharp2@gmail.com>
2020-05-24 23:19:55 +0300

    sockets

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -5,6 +5,8 @@ function connectSockets(io) {
 
     io.on('connection', socket => {
         socket.on('doRefresh', msg=>{
+            console.log('doing refresh')
+
             io.to(socket.boardId).emit('doRefresh', msg)
         })
         socket.on('boardViewed', boardId=>{
@@ -15,5 +17,30 @@ function connectSockets(io) {
             socket.join(boardId)
             socket.boardId = boardId;
         })
+
+
+
+        socket.on('boardChat', chatBoardId=>{
+            if (socket.chatBoardId) {
+                socket.leave(socket.chatBoardId)
+            }
+            socket.join(chatBoardId)
+            socket.chatBoardId = chatBoardId;
+            console.log('some1 logged in')
+        })
+
+
+
+        socket.on('sendMsg', msg=>{
+            console.log('msg')
+            socket.broadcast.emit('sendMsg', msg)
+            // io.to(socket.chatBoardId).emit('sendMsg', msg)
+            // socket.broadcast.to(socket.chatBoardId).emit('sendMsg', msg);
+            // io.to(socket.chatBoardId).emit('newMsg', msg);
+        })
+        
+        
+
+
     })
 }
