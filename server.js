97fea00b3b2ff640abce59b6a9b3fcfb9a37aa3e
AbirNadav <pakakaw41@gmail.com>
2020-05-27 17:52:21 +0300

    firstapp!!

--- server.js
+++ server.js
@@ -49,6 +49,10 @@ connectSockets(io)
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030;
+app.get('*', function (req, res) {
+    res.redirect('/')
+})
+
 http.listen(port, () => {
     logger.info('Server is running on port: ' + port)
 });
