83865fbe27d3e67632bd4e0aea941d40c6a07f16
AbirNadav <pakakaw41@gmail.com>
2020-05-27 17:25:36 +0300

    firstapp!!

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -5,7 +5,7 @@ async function getUser(req, res) {
     const user = await userService.getById(req.params.id)
     res.send(user)
 }
-  
+
 async function getUsers(req, res) {
     // console.log(req.query);
     const users = await userService.query(req.query)
---
+++ public/precache-manifest.4ca56866509b4bcfbbad39ddf1d4cebb.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87d2f370095d410687a320074b8817a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7dfe1ad210476a673b7",
+    "url": "/static/css/2.0ced7506.chunk.css"
+  },
+  {
+    "revision": "df2630ed10c9729d9ca8",
+    "url": "/static/css/main.07a21d22.chunk.css"
+  },
+  {
+    "revision": "e7dfe1ad210476a673b7",
+    "url": "/static/js/2.1a6981e0.chunk.js"
+  },
+  {
+    "revision": "a1a851c5c64319cd4c861d010e3d24f8",
+    "url": "/static/js/2.1a6981e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2630ed10c9729d9ca8",
+    "url": "/static/js/main.d5ecb8e7.chunk.js"
+  },
+  {
+    "revision": "798f76c80dcadeb6cc12",
+    "url": "/static/js/runtime-main.5d63068a.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans.3ed9575d.ttf"
+  },
+  {
+    "revision": "f6c2e43363929a8f26be71af3af1e1da",
+    "url": "/static/media/calendar.f6c2e433.svg"
+  },
+  {
+    "revision": "e0d2b7486b532288a980d0cd5d535340",
+    "url": "/static/media/chat-icon.e0d2b748.svg"
+  },
+  {
+    "revision": "63e1084d1eb86f13b61d7d7987b121a8",
+    "url": "/static/media/chat.63e1084d.png"
+  },
+  {
+    "revision": "2d88032616169443464aa40e1b2d088a",
+    "url": "/static/media/checkbox.2d880326.png"
+  },
+  {
+    "revision": "bff23160bc0a852804aea40ca3bca32a",
+    "url": "/static/media/delete.bff23160.svg"
+  },
+  {
+    "revision": "222dd2dacb8dae2eb20f65ea820e3e07",
+    "url": "/static/media/invitation.222dd2da.svg"
+  },
+  {
+    "revision": "7718b3e3ff59dac728c55e725a3b0523",
+    "url": "/static/media/logo-no-bg.7718b3e3.svg"
+  },
+  {
+    "revision": "f2103d04e38fd4f5b096c7b008e62896",
+    "url": "/static/media/mail.f2103d04.svg"
+  },
+  {
+    "revision": "0d062b33a8b60acb2a497afe63666b16",
+    "url": "/static/media/notification.0d062b33.mp3"
+  },
+  {
+    "revision": "d6d14d5845e5e38bdf0d496191239260",
+    "url": "/static/media/notification.d6d14d58.svg"
+  },
+  {
+    "revision": "6cda1fcfdd3fc4a8c4554fca36f798d6",
+    "url": "/static/media/pencil.6cda1fcf.svg"
+  },
+  {
+    "revision": "64c0cf279631123f30bbf66fb538d747",
+    "url": "/static/media/person.64c0cf27.svg"
+  }
+]);
